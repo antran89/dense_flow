5390f79bdd4c7eba01d54d602ccfddc7080fba84
antran89 <tranlaman@gmail.com>
2017-01-04 14:27:26 +0800

    add another video test

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -9,4 +9,5 @@ if [ ! -d $FLOW_FOLDER ]; then
 	mkdir $FLOW_FOLDER
 fi
 
+../src-build/denseFlow_gpu -f=v_ApplyEyeMakeup_g14_c04.avi -i=img_folder/im -x=flow_folder/flow_x -y=flow_folder/flow_y -b=20 -t=1 -s=2
 ../src-build/denseFlow_gpu -f=Megamind_bugy.avi -i=img_folder/im -x=flow_folder/flow_x -y=flow_folder/flow_y -b=20 -t=1 -s=2
