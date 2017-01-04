53beed46d8a1c6fcd297e4a9e546d2a0f499cb83
antran89 <tranlaman@gmail.com>
2017-01-04 17:52:34 +0800

    add <none> in CommandLineParser

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -9,5 +9,5 @@ if [ ! -d $FLOW_FOLDER ]; then
 	mkdir $FLOW_FOLDER
 fi
 
-../src-build/denseFlow_gpu -f=v_ApplyEyeMakeup_g14_c04.avi -i=img_folder/im -x=flow_folder/flow_x -y=flow_folder/flow_y -b=20 -t=1 -s=2
-../src-build/denseFlow_gpu -f=Megamind_bugy.avi -i=img_folder/im -x=flow_folder/flow_x -y=flow_folder/flow_y -b=20 -t=1 -s=2
+../src-build/denseFlow_gpu -f=v_ApplyEyeMakeup_g14_c04.avi -i=$IMG_FOLDER/im -x=$FLOW_FOLDER/flow_x -y=$FLOW_FOLDER/flow_y -b=20 -t=1 -s=2
+../src-build/denseFlow_gpu -f=Megamind_bugy.avi -i=$IMG_FOLDER/im -x=$FLOW_FOLDER/flow_x -y=$FLOW_FOLDER/flow_y -b=20 -t=1 -s=2
